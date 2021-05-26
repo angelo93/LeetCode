26c868d73925003f71dd0c3607ea246aa6c53065
Michael Navarro <navarro.michael93@gmail.com>
2021-05-25 23:41:07 -0500

    Finish Palindrome Number Solution

--- Easy/#0009. Palindrome Number.py
+++ Easy/#0009. Palindrome Number.py
@@ -1,9 +1,9 @@
 class Solution:
     def isPalindrome(self, x):
         '''
-        type x: int
-        type rev_num: int
-        rtype result: bool
+        type x:         int
+        type rev_num:   int
+        rtype result:   bool
         '''
 
         rev_num = int()
@@ -12,7 +12,6 @@ class Solution:
         # Check edge cases
         if x < 0 or x % 10 == 0:
             result = False
-            rev_num = -1
         else:
             rev_num = self.getReverse(x)
         # end if
@@ -29,18 +28,25 @@ class Solution:
 
     def getReverse(self, x):
         '''
-        type x: int
-        rtype rev_num: int
+        type x:             int
+        type num_to_add:    int
+        rtype rev_num:      int
         '''
 
         rev_num = 0
 
         # While x > 0
-        # Isolate last number
-        # Create ones place
-        # Add last number to ones place
-        # Reduce original number by ones place
+        while x > 0:
+            num_to_add = x % 10     # Isolate ones place
 
+            # Update reversed number
+            rev_num = rev_num * 10  # Create new ones place
+            rev_num += num_to_add   # Add isolated number to ones place
+            
+            # Update original number
+            x = x // 10             # Remove current last number
+        # end while
+        
         return rev_num
     # end def getReverse
 # end class Solution
