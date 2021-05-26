85c10297a22564fb23d8332bf6e57219b51b2187
Michael Navarro <navarro.michael93@gmail.com>
2021-05-25 23:25:25 -0500

    Outline Palindrome Number

---
+++ Easy/#0009. Palindrome Number.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isPalindrome(self, x):
+        '''
+        type x: int
+        type rev_num: int
+        rtype result: bool
+        '''
+
+        result = bool()
+
+        # Check if num is less than 0 or ends in 0
+            # If so, result = False
+            # Reverse = -1
+        # else 
+            # Get reverse
+        
+        # Check if reverese = original
+            # If so, result = True
+            # If not, result = False
+
+
+        return result
+    # end def isPalindrome
+
+    def getReverse(self, x):
+        '''
+        type x: int
+        rtype rev_num: int
+        '''
+
+        rev_num = 0
+
+        # While x > 0
+        # Isolate last number
+        # Create ones place
+        # Add last number to ones place
+        # Reduce original number by ones place
+
+        return rev_num
+    # end def getReverse
+# end class Solution
