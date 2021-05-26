d236699c06f34181dcccaa563ef14d9b1928c8e3
Michael Navarro <navarro.michael93@gmail.com>
2021-05-25 21:37:12 -0500

    Document and refactor Two Sums

---
+++ Easy/#0001. Two Sums.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :type compliment: Dict{int: int}
+        :rtype result: List[int]
+        """
+
+        compliment = {}  # dictionary of possible complimentary numbers
+        result = list() # variable to be returned
+
+        # For every index and number in list nums
+        for idx, num in enumerate(nums):
+            # Check if complimetary number exists
+            if num in compliment:                
+                result = [compliment[num], idx]
+            # else, record current number's compliment and index
+            else:
+                compliment[target - num] = idx
+            #end if
+        # end for
+
+        return result
+    # end def TwoSum
+# end class Solution
